f141ea148dc349876814aa4bde423a0d284cf4d4
Jan C Kaldestad <janck76@gmail.com>
2016-09-29 23:58:51 +0200

    Fix typo

--- resources/logback.groovy
+++ resources/logback.groovy
@@ -1,9 +1,7 @@
 def USER_HOME = System.getProperty("user.home");
-println "*** logback.grooCY loaded ***"
-// no.kommune.bergen.omi.notification
+println "*** logback.groovy loaded ***"
 
 appender("FILE", FileAppender) {
-  // println "Setting [file] property to [${USER_HOME}/myApp.log]"
   file = "${USER_HOME}/git/groovy-slf4j/test.log"
   encoder(PatternLayoutEncoder) {
     // pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS} %msg%n"
